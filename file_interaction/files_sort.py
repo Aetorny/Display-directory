14763b0daf8a989c0c6a08c8cabfc5c7a3f3579d
Aetorny <smgusak@gmail.com>
2023-05-17 19:09:59 +0300

    modified:   check_to_help.py deleted:    file_interaction/__pycache__/file_class.cpython-311.pyc deleted:    file_interaction/__pycache__/file_filter_sifting.cpython-311.pyc deleted:    file_interaction/__pycache__/files_displayer.cpython-311.pyc deleted:    file_interaction/__pycache__/files_giver.cpython-311.pyc deleted:    file_interaction/__pycache__/files_parser.cpython-311.pyc deleted:    file_interaction/__pycache__/files_sort.cpython-311.pyc modified:   file_interaction/files_sort.py

--- file_interaction/files_sort.py
+++ file_interaction/files_sort.py
@@ -4,13 +4,13 @@ from file_interaction.file_class import Files
 
 
 def sort_files(files: Files) -> None:
-    if '-m' in argv or '-mt' in argv or '--modificationtime' in argv:
-        if '-d' in argv or '--descending' in argv:
+    if '-m' in argv or '-mt' in argv or '-modificationtime' in argv:
+        if '-d' in argv or '-descending' in argv:
             files.sort(('modification_date', 'descending'))
         else:
             files.sort(('modification_date', 'ascending'))
         return
-    if '-d' in argv or '--descending' in argv:
+    if '-d' in argv or '-descending' in argv:
         files.sort(('name', 'descending'))
     else:
         files.sort(('name', 'ascending'))
