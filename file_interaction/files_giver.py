0020597fff633dd913ba873b1227300bdbcb7dd4
Aetorny <smgusak@gmail.com>
2023-05-17 18:48:21 +0300

    new file:   .gitignore modified:   file_interaction/files_giver.py

--- file_interaction/files_giver.py
+++ file_interaction/files_giver.py
@@ -6,8 +6,4 @@ from directory_information import DISPLAY_DIRECTORY
 
 def get_files() -> List[str]:
     files = os.listdir(DISPLAY_DIRECTORY)
-    if '.mypy_cache' in files:
-        files.remove('.mypy_cache')
-    if '__pycache__' in files:
-        files.remove('__pycache__')
     return files
